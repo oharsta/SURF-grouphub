7bd32b7042c735c434cab14919e95aab1a620770
oharsta <oharsta@zilverline.com>
2016-02-15 13:45:02 +0100

    New release to github

--- build/javascripts/App-ebf1eb71.js
+++ build/javascripts/App-ebf1eb71.js
@@ -12,6 +12,7 @@ class App {
     this.sortingSelector();
     this.languageSelector();
     this.modals();
+    this.editGroup();
   }
 
   toggleHidden(e, id, force) {
@@ -36,9 +37,11 @@ class App {
 
   groupsSelector() {
     ['all_groups', 'organisation_groups', 'my_groups', 'search'].forEach((key) => {
+      let section = document.getElementById('group_' + key);
+      section.style.height = (screen.height - 200) + 'px';
       let link = document.querySelector('#close_' + key);
       link.addEventListener('click', (e) => {
-        this.toggleHidden(e,'#group_' + key, true);
+        this.toggleHidden(e, '#group_' + key, true);
         //need to unselect the checkbox for state consistency
         let checkbox = document.querySelector('#select_' + key);
         if (checkbox) {
@@ -72,8 +75,52 @@ class App {
     });
 
     let editLinks = Array.from(document.querySelectorAll('.button_edit'));
-    editLinks.forEach((link) => link.addEventListener('click', (e) => toggleModal(e,'#edit_group')));
+    editLinks.forEach((link) => link.addEventListener('click', (e) => toggleModal(e, '#edit_group')));
     document.querySelector('#edit_group_close').addEventListener('click', (e) => toggleModal(e, '#edit_group'));
+
+    let joinLinks = Array.from(document.querySelectorAll('.button_join'));
+    joinLinks.forEach((link) => link.addEventListener('click', (e) => toggleModal(e, '#join_group')));
+    document.querySelector('#join_group_close').addEventListener('click', (e) => toggleModal(e, '#join_group'));
+  }
+
+  editGroup() {
+    document.getElementById('show_group_details').addEventListener('click', (e) => {
+      this.stop(e);
+      document.getElementById('group_details').classList.toggle('hidden');
+    });
+    document.getElementById('edit_group_link').addEventListener('click', (e) => {
+      this.stop(e);
+      document.getElementById('group_title').classList.toggle('hidden');
+      document.getElementById('edit_group_title').classList.toggle('hidden');
+      document.getElementById('group_details').classList.toggle('hidden', true);
+      document.getElementById('edit_group_details').classList.toggle('hidden');
+    });
+    document.getElementById('delete_group_link').addEventListener('click', (e) => {
+      this.stop(e);
+      document.getElementById('group_deletion_confirmation').classList.toggle('hidden');
+    });
+    document.getElementById('add_members').addEventListener('click', (e) => {
+      this.stop(e);
+      this.toggleTab('group_members', false);
+      this.toggleTab('add_members', true);
+    });
+    document.getElementById('group_members').addEventListener('click', (e) => {
+      this.stop(e);
+      this.toggleTab('group_members', true);
+      this.toggleTab('add_members', false);
+    });
+  }
+
+  toggleTab(tabName, active) {
+    var classListLink = document.getElementById(tabName).classList;
+    var classListTab = document.getElementById(tabName + '_tab').classList;
+    if (active) {
+      classListLink.add('active');
+      classListTab.remove('hidden');
+    } else {
+      classListLink.remove('active');
+      classListTab.add('hidden');
+    }
   }
 
   languageSelector() {
